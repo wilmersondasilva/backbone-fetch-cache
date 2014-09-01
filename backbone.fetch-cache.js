d6a73e9142e06f8ed952f4319fb7fe98983ce4b1
Administrator <admin@local.host>
2014-09-01 19:06:40 +0200

    Fixed identation

--- backbone.fetch-cache.js
+++ backbone.fetch-cache.js
@@ -5,19 +5,19 @@
 
 // AMD wrapper from https://github.com/umdjs/umd/blob/master/amdWebGlobal.js
 
-(function(root, factory) {
-		if (typeof define === 'function' && define.amd) {
-			// AMD. Register as an anonymous module and set browser global
-			define(['underscore', 'backbone', 'jquery'], function(_, Backbone, $) {
-				return (root.Backbone = factory(_, Backbone, $));
-			});
-		} else if (typeof exports !== 'undefined') {
-			module.exports = factory(require('underscore'), require('backbone'), require('jquery'));
-		} else {
-			// Browser globals
-			root.Backbone = factory(root._, root.Backbone, root.jQuery);
-		}
-	}(this, function (_, Backbone, $) {
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module and set browser global
+    define(['underscore', 'backbone', 'jquery'], function (_, Backbone, $) {
+      return (root.Backbone = factory(_, Backbone, $));
+    });
+  } else if (typeof exports !== 'undefined') {
+    module.exports = factory(require('underscore'), require('backbone'), require('jquery'));
+  } else {
+    // Browser globals
+    root.Backbone = factory(root._, root.Backbone, root.jQuery);
+  }
+}(this, function (_, Backbone, $) {
 
   // Setup
   var superMethods = {
